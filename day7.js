ffd485869b4d244c54029394a90556223c7d3776
UmeshMehta1 <ukmehta1245@gmail.com>
2025-08-24 17:37:48 +0545

    day 7

---
+++ day7.js
@@ -0,0 +1,25 @@
+const mapArr = [2, 4, 7, 1, 3, 0, 4, 2, 6]
+
+
+// const result = mapArr.map( (item, index)=> item*12)
+   
+
+// const resutFilter = mapArr.filter((item)=> {
+//      return item%2==0
+// } )
+
+
+
+
+// console.log(result)
+// console.log(resutFilter)
+
+
+const redArry = [1, 5, 10, 20, 30]
+
+// const reduceResul = redArry.reduce((TotalVal, currentValue)=>{
+          
+//            return TotalVal +currentValue
+// },100)
+
+// console.log(reduceResul)
